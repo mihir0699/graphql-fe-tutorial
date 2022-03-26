27d6c45bb9f6bf487c43ed329653a8703a8b83ba
Mihir Gupta <mihir0699@gmail.com>
2022-03-26 19:25:48 +0530

    initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,25 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { ChakraProvider } from "@chakra-ui/react";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  useQuery,
+  gql,
+} from "@apollo/client";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const client = new ApolloClient({
+    uri: "http://localhost:8080/graphql",
+    cache: new InMemoryCache(),
+  });
+  return (
+    <ApolloProvider client={client}>
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </ApolloProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
